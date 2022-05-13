7256a324c14df956fb73f791b5c24d3d2d7ec61c
youngwoo <lyw960820@icloud.com>
2022-05-13 17:17:02 +0900

    commit

--- js/index.js
+++ js/index.js
@@ -50,21 +50,18 @@ function remove() {
 
 setTimeout(type, 2200);
 
-//여기부터 하면됨. 220511-2112 html 하나의 서클이니까 여러개로 맞춰서 코딩
-var barHtml = document.querySelector(".bar-html");
-var barCss = document.querySelector(".bar-css");
-var barReact = document.querySelector(".bar-react");
-var barJs = document.querySelector(".bar-js");
-var barTs = document.querySelector(".bar-ts");
-var barJava = document.querySelector(".bar-java");
+const barHtml = document.querySelector(".bar-html");
+const barCss = document.querySelector(".bar-css");
+const barReact = document.querySelector(".bar-react");
+const barJs = document.querySelector(".bar-js");
+const barTs = document.querySelector(".bar-ts");
+const barJava = document.querySelector(".bar-java");
 
-
-
-var RADIUS = 15.5;
-var CIRCUMFERENCE = 2 * Math.PI * RADIUS;
+let RADIUS = 15.5;
+let CIRCUMFERENCE = 2 * Math.PI * RADIUS;
 
 function progress(html, css, react, js, ts, java) {
-  var progress = [
+  const progress = [
     html / 100,
     css / 100,
     react / 100,
@@ -72,7 +69,7 @@ function progress(html, css, react, js, ts, java) {
     ts / 100,
     java / 100,
   ];
-  var dashoffset = [
+  const dashoffset = [
     CIRCUMFERENCE * (1 - progress[0]),
     CIRCUMFERENCE * (1 - progress[1]),
     CIRCUMFERENCE * (1 - progress[2]),
@@ -87,14 +84,100 @@ function progress(html, css, react, js, ts, java) {
   barJs.style.strokeDashoffset = dashoffset[3];
   barTs.style.strokeDashoffset = dashoffset[4];
   barJava.style.strokeDashoffset = dashoffset[5];
-  
 }
 
 barHtml.style.strokeDasharray = CIRCUMFERENCE;
 barCss.style.strokeDasharray = CIRCUMFERENCE;
-barReact.style.strokeDasharray =CIRCUMFERENCE;
-barJs.style.strokeDasharray =CIRCUMFERENCE;
-barTs.style.strokeDasharray =CIRCUMFERENCE;
-barJava.style.strokeDasharray =CIRCUMFERENCE;
+barReact.style.strokeDasharray = CIRCUMFERENCE;
+barJs.style.strokeDasharray = CIRCUMFERENCE;
+barTs.style.strokeDasharray = CIRCUMFERENCE;
+barJava.style.strokeDasharray = CIRCUMFERENCE;
 
 progress(90, 90, 80, 80, 50, 70);
+
+const valueHtml = document.querySelector(".value-html");
+const valueCss = document.querySelector(".value-css");
+const valueReact = document.querySelector(".value-react");
+const valueJs = document.querySelector(".value-js");
+const valueTs = document.querySelector(".value-ts");
+const valueJava = document.querySelector(".value-java");
+
+function overHtml() {
+  valueHtml.innerHTML = `<span style="font-weight:400; font-size:0.9rem; text-align:center">웹표준 기준 태그 <br> 시멘틱 태그, SVG <br> Canvas에 대한 이해 </span>`;
+}
+
+function leaveHtml() {
+  valueHtml.innerHTML = `<h4>HTML</h4><span>90%</span>`;
+}
+
+function overCss() {
+  valueCss.innerHTML = `<span style="font-weight:400; font-size:0.9rem; text-align:center">레이아웃, 스타일시트 <br> 미디어 쿼리, 애니메이션 <br> SCSS에 대한 이해</span>`;
+}
+
+function leaveCss() {
+  valueCss.innerHTML = `<h4>CSS</h4><span>90%</span>`;
+}
+
+function overReact() {
+  valueReact.innerHTML = `<span style="font-weight:400; font-size:0.9rem; text-align:center">React Hooks <br> Component <br> State에 대한 이해</span>`;
+}
+
+function leaveReact() {
+  valueReact.innerHTML = `<h4>React</h4><span>90%</span>`;
+}
+function overJs() {
+  valueJs.innerHTML = `<span style="font-weight:400; font-size:0.9rem; text-align:center">ES6 이상의 문법 <br> Event, Promise <br> prototype에 대한 이해</span>`;
+}
+
+function leaveJs() {
+  valueJs.innerHTML = `<h4>JavaScript</h4><span>90%</span>`;
+}
+function overTs() {
+  valueTs.innerHTML = `<span style="font-weight:400; font-size:0.9rem; text-align:center">Object, Return  <br> Interface, Union <br> 상속에 대한 이해</span>`;
+}
+
+function leaveTs() {
+  valueTs.innerHTML = `<h4>TypeScript</h4><span>90%</span>`;
+}
+
+function overJava() {
+  valueJava.innerHTML = `<span style="font-weight:400; font-size:0.9rem; text-align:center">객체지향 프로그래밍  <br> Spring 프레임워크 <br> JSP에 대한 이해</span>`;
+}
+
+function leaveJava() {
+  valueJava.innerHTML = `<h4>Java</h4><span>90%</span>`;
+}
+
+// $(document).ready(function ($) {
+//   $(".scroll-move").click(function (event) {
+//     event.preventDefault();
+
+//     $("html,body").animate({ scrollTop: $(this.hash).offset().top }, 500);
+//   });
+// });
+
+$(document).on("click", ".nav-item a", function (event) {
+  event.preventDefault();
+  $("html,body").animate(
+    {
+      scrollTop: $(this.hash).offset().top - 150
+    },
+    500
+  );
+});
+
+function navigo() {
+  const header = document.querySelector("header");
+  document.addEventListener("scroll", onScroll, { passive: true });
+  function onScroll() {
+    const scrollposition = scrollY;
+
+    const nav = document.querySelector(".navbar");
+    if (header.clientHeight <= scrollposition) {
+      nav.classList.add("fix");
+    } else {
+      nav.classList.remove("fix");
+    }
+  }
+}
+navigo();
